a16e4e9bd0a339bcb1ebde4902964114c837b0f3
Hope Skinner <hope.skinner@oracle.com>
2021-08-23 11:17:30 -0400

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=6dfb2655f6ed5b8cf11ac32ad4be9d9c&query='+ latitude + ',' + longitude + '&units=f'
+//console.log(url)
+    request({url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined) 
+        } else if (!body.current) {
+            callback('Unable to find location.  Try another search.', undefined)
+        } else {
+            const data = body.current
+          
+            callback(undefined, 
+                data.weather_descriptions[0] + ' right now.  It is currently '+ data.temperature + ' degrees.  It feels like ' + data.feelslike +'.'
+            )
+        }
+    }) 
+}
+
+module.exports = forecast
